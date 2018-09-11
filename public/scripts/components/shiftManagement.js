ceba16c19af294b52d02a283fd5a3bed9c362017
Itamar Davidyan <itamardavidyan@gmail.com>
2018-09-11 10:26:30 +0300

    node

--- public/scripts/components/shiftManagement.js
+++ public/scripts/components/shiftManagement.js
@@ -1,47 +1,36 @@
 var dvalue = -1;
-var csv = 'Name, ID\n';
 
-function createCSV() {
-    console.log("createCSV: " + csv);
+function createCSV(csv) {
+    console.log("createCSV:");
+    console.log(csv);
     var hiddenElement = document.createElement('a');
-    hiddenElement.href = 'data:text/csv;charset=utf-8,' + encodeURI(csv);
+    hiddenElement.href = 'data:text/csv;charset=utf-8,%EF%BB%BF' + encodeURI(csv);
     hiddenElement.target = '_blank';
     hiddenElement.download = 'Users.csv';
     hiddenElement.click();
 }
 
 function createArray() {
+    var csv = 'תעודת זהות, שם מלא, ערך,הערות\n';
     var deferred = $.Deferred();
     console.log("createArray")
-    var fullArr = [];
 
     db.collection("users").get().then(function(querySnapshot) {
         querySnapshot.forEach(function(doc) {
-            // doc.data() is never undefined for query doc snapshots
-            // console.log(doc.id, " => ", doc.data());
-            var arr = [doc.data().name, doc.data().value];
-            fullArr.push(arr);
-            csv += doc.data().name + ',' + doc.data().value + '\n';
-            console.log(csv);
+            console.log(doc.data());
+            csv += doc.data().id + ',' + doc.data().name + ',' + doc.data().value + ',' + doc.data().comment + '\n';
         });
+        createCSV(csv);
     });
-    console.log(fullArr);
-
-    return deferred.promise();
 }
 
 export default class ShiftManagement {
     init(el) {
         el.innerHTML = `
-            <div id="shiftManagement">
-                <aside class="toolbar" dir="rtl">
-                    <p class="lead" id="dname">שם:</p>
-                    <p class="lead" id="disValue">ערך:</p>
-                </aside>
-
-                <table class="table table-striped table-bordered" align="right">
-                    <thead>
-                      <tr>
+            <p class="lead" id="dname">שם משתמש:</p>
+            <table class="table table-striped table-bordered" id="shiftTable">
+                <thead>
+                    <tr>
                         <th>יום שבת</th>
                         <th>יום שישי</th>
                         <th>יום חמישי</th>
@@ -50,10 +39,10 @@ export default class ShiftManagement {
                         <th>יום שני</th>
                         <th>יום ראשון</th>
                         <th></th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      <tr>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
                         <td><button type="button" class="btn inner" id="inner_button" name="0"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="1"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="2"> </button></td>
@@ -61,9 +50,9 @@ export default class ShiftManagement {
                         <td><button type="button" class="btn inner" id="inner_button" name="4"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="5"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="6"> </button></td>
-                        <th>בוקר</th>
-                      </tr>
-                      <tr>
+                        <td class="tdBold">בוקר</td>
+                    </tr>
+                    <tr>
                         <td><button type="button" class="btn inner" id="inner_button" name="7"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="8"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="9"> </button></td>
@@ -71,9 +60,9 @@ export default class ShiftManagement {
                         <td><button type="button" class="btn inner" id="inner_button" name="11"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="12"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="13"> </button></td>
-                        <th>ערב</th>
-                      </tr>
-                      <tr>
+                        <td class="tdBold">ערב</td>
+                    </tr>
+                    <tr>
                         <td><button type="button" class="btn inner" id="inner_button" name="14"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="15"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="16"> </button></td>
@@ -81,17 +70,44 @@ export default class ShiftManagement {
                         <td><button type="button" class="btn inner" id="inner_button" name="18"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="19"> </button></td>
                         <td><button type="button" class="btn inner" id="inner_button" name="20"> </button></td>
-                        <th>לילה</th>
-                      </tr>
-                    </tbody>
-                </table>
-
+                        <td class="tdBold">לילה</td>
+                    </tr>
+                </tbody>
+            </table>
+                
+            <div class="comment-group" dir="rtl">
+                <label for="comment">הערות:</label>
+                <textarea class="form-control" rows="2" id="comment"></textarea>
             </div>
 
             <button id="btn-save" type="button" class="btn btn-success">שמור</button>
             <button id="btn-signout" type="button" class="btn btn-danger">שמור והתנתק</button>
+            <button id="export" type="button" class="btn btn-info">ייצא</button>
+            <button id="info" type="button" class="btn btn-info"><i class="fa fa-info-circle" aria-hidden="true"></i></button>
             <br>
-            <button id="export" type="button" class="btn">export</button>
+            <p id="saveMsg">המשמרות נשמרו בהצלחה!</p>
+
+            <!-- The Modal -->
+            <div id="myModal" class="modal">
+            <!-- Modal content -->
+            <div class="modal-content">
+                <div class="modal-header">
+                    <span class="close" id="closeBtn">&times;</span>
+                    <h3>רשימת בקרים:</h3>
+                </div>
+                <div class="modal-body">
+                    <h4 style="color: red;">בקרים שלא הזינו משמרות:</h2>
+                    <ul id="submitF">
+                        <li>Coffee</li>
+                    </ul>
+                    <h4 style="color: green;">בקרים שהזינו משמרות:</h2>
+                    <ul id="submitT">
+                        <li>Coffee</li>
+                    </ul>    
+                </div>
+            </div>
+
+            </div>
         `;
 
         $('#btn-signout').on('click', function() {
@@ -131,15 +147,36 @@ export default class ShiftManagement {
         });
 
         $('#export').on('click', function() {
-/*            $.when(createArray()).done(function() {
-                alert("1");
-                createCSV();
-            });*/
-            createArray().then(function() {
-                alert("2");
-                createCSV();
+            createArray();
+        });
+
+        $('#info').on('click', function() {
+            var today = new Date();
+            var thisWeek = today.getWeek();
+
+            $("#submitF").empty();
+            $("#submitT").empty();
+
+            $("#myModal").css("display", "block");
+
+            firebase.firestore().collection('users').get().then(function(querySnapshot) {
+                querySnapshot.forEach(function(docu) {
+                    if (docu.data().week == thisWeek) $("#submitT").append("<li>" + docu.data().name + "</li>");
+                    else $("#submitF").append("<li>" + docu.data().name + "</li>");
+                });
             });
         });
+
+        $('#closeBtn').on('click', function() {
+            $("#myModal").css("display", "none");
+        });
+
+        window.onclick = function(event) {
+            var modal = document.getElementById('myModal');
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
     }
 } 
 
@@ -150,22 +187,25 @@ db.settings(settings);
 
 firebase.auth().onAuthStateChanged(function(user) {
     if (user) {
-        const name = user.displayName;
-        const value = user.value;
         const id   = user.uid;
-        // alert(`Welcome! ${name}. Your id isssssssssssssssssssssss ${id}`);
+
         $(document).ready(function(){
-            $("#dname").text("שם משתמש: " + name);
             firebase.firestore().collection('users').doc(id).get().then(function(doc) {
                 setDvalue(doc.data().value, setButtons);
-                $("#disValue").text("ערך: " + dvalue);
+                $("#comment").text(doc.data().comment);
 
+                const name = doc.data().name;
+                $("#dname").text("שם משתמש: " + name);
+                if (name == 'איתמר דודיאן' || name == 'שירה צוקרמן'){
+                    $('#export').css("display", "inline");
+                    $('#info').css("display", "inline");
+                }
             });
         });
     } else {
         // user not signed in, do nothing
     }
-}); 
+});
 
 function setDvalue(value, callback) {
     // console.log("hi" + value);
@@ -174,7 +214,6 @@ function setDvalue(value, callback) {
 }
 
 function setButtons() {
-    // console.log("hi" + dvalue);
     var value = dvalue;
 
     var buttons = document.getElementsByTagName('button');
@@ -183,7 +222,6 @@ function setButtons() {
 
         var id = button.getAttribute('id');
         if (id == "inner_button") {
-            // button.innerHTML = 'hi' + i;
             if (shiftCheck(value)) button.style.background='#008000';
             else button.style.background='#FF0000';
             value = Math.floor(value / 2);
@@ -200,10 +238,26 @@ function shiftCheck(shiftValue) {
 function save() {
     if (dvalue == -1) return;
 
+    var today = new Date();
+    var week = today.getWeek();
+
+    var comment = $("#comment").val();
     const value = dvalue;
     const userid = firebase.auth().currentUser.uid;
     firebase.firestore().collection('users').doc(userid).update({
-      value,
+      value, comment, week
+    }).then(function(){
+        $('#saveMsg').css("display", "block");
+        $('#saveMsg').delay(2500).fadeOut();
+    }).catch(function(error) {
+        alert('error:\n' + error);
     });
 }
+
+Date.prototype.getWeek = function() {
+    var onejan = new Date(this.getFullYear(),0,1);
+    var today = new Date(this.getFullYear(),this.getMonth(),this.getDate());
+    var dayOfYear = ((today - onejan +1)/86400000);
+    return Math.ceil(dayOfYear/7)
+};
 
